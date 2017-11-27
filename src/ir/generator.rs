4e17093933e307ef96b0ae6ab1e531920a6ce01e
Patrick Koenig <pkoenig10@gmail.com>
2017-11-26 22:36:17 -0800

    Clean up IR generator

--- src/ir/generator.rs
+++ src/ir/generator.rs
@@ -25,22 +25,18 @@ impl<'input> IrGenerator<'input> {
         }
     }
 
-    fn builder(&mut self) -> &mut IrBuilder {
-        &mut self.builder
-    }
-
     fn generate_func(mut self, func: &ast::Func<'input>) -> Func {
         self.symbol_table.push_scope();
 
         for param in func.params() {
             let type_ = self.generate_type(param.type_());
-            let value = self.builder().create_param(type_);
+            let value = self.builder.create_param(type_);
             self.insert_symbol(param.identifier(), value);
         }
 
-        let block_id = self.builder().create_block();
-        self.builder().push_block(block_id);
-        self.builder().set_insert_block(block_id);
+        let block_id = self.builder.create_block();
+        self.builder.push_block(block_id);
+        self.builder.set_insert_block(block_id);
 
         self.generate_stmt(func.stmt());
 
@@ -71,7 +67,7 @@ impl<'input> IrGenerator<'input> {
     }
 
     fn generate_return_stmt(&mut self, _stmt: &ast::ReturnStmt) {
-        self.builder().push_return_inst();
+        self.builder.push_return_inst();
     }
 
     fn generate_block_stmt(&mut self, stmt: &ast::BlockStmt<'input>) {
@@ -99,17 +95,17 @@ impl<'input> IrGenerator<'input> {
 
     fn generate_int_literal(&mut self, int_literal: &ast::IntLiteral) -> Value {
         let value = IntImmediate::new(int_literal.value());
-        self.builder().push_int_const_inst(value)
+        self.builder.push_int_const_inst(value)
     }
 
     fn generate_float_literal(&mut self, float_literal: &ast::FloatLiteral) -> Value {
         let value = FloatImmediate::new(float_literal.value());
-        self.builder().push_float_const_inst(value)
+        self.builder.push_float_const_inst(value)
     }
 
     fn generate_bool_literal(&mut self, bool_literal: &ast::BoolLiteral) -> Value {
         let value = BoolImmediate::new(bool_literal.value());
-        self.builder().push_bool_const_inst(value)
+        self.builder.push_bool_const_inst(value)
     }
 
     fn generate_identifier(&mut self, identifier: &ast::Identifier) -> Value {
@@ -120,25 +116,27 @@ impl<'input> IrGenerator<'input> {
         let src_value = self.generate_expr(expr.expr());
 
         let op = expr.op();
-        let src_type = self.builder().get_value_type(src_value);
-
-        let builder = self.builder();
+        let src_type = self.builder.get_value_type(src_value);
 
         let src = Operand::Value(src_value);
 
         match (op, src_type) {
             (ast::Negate, Int) => {
-                builder.push_binary_inst(Sub, Operand::IntImmediate(IntImmediate::new(0)), src)
+                self.builder.push_binary_inst(
+                    Sub,
+                    Operand::IntImmediate(IntImmediate::new(0)),
+                    src,
+                )
             }
             (ast::Negate, Float) => {
-                builder.push_binary_inst(
+                self.builder.push_binary_inst(
                     Fsub,
                     Operand::FloatImmediate(FloatImmediate::new(0.)),
                     src,
                 )
             }
             (ast::BitNot, Int) |
-            (ast::LogicalNot, Bool) => builder.push_unary_inst(Not, src),
+            (ast::LogicalNot, Bool) => self.builder.push_unary_inst(Not, src),
             _ => {
                 panic!(
                     "Invalid unary expression '{}' with operand type '{}'",
@@ -154,41 +152,39 @@ impl<'input> IrGenerator<'input> {
         let right_value = self.generate_expr(expr.right());
 
         let op = expr.op();
-        let left_type = self.builder().get_value_type(left_value);
-        let right_type = self.builder().get_value_type(right_value);
-
-        let builder = self.builder();
+        let left_type = self.builder.get_value_type(left_value);
+        let right_type = self.builder.get_value_type(right_value);
 
         let left = Operand::Value(left_value);
         let right = Operand::Value(right_value);
 
         match (op, left_type, right_type) {
-            (ast::Mul, Float, Float) => builder.push_binary_inst(Fmul, left, right),
-            (ast::Add, Int, Int) => builder.push_binary_inst(Add, left, right),
-            (ast::Add, Float, Float) => builder.push_binary_inst(Fadd, left, right),
-            (ast::Sub, Int, Int) => builder.push_binary_inst(Sub, left, right),
-            (ast::Sub, Float, Float) => builder.push_binary_inst(Fsub, left, right),
-            (ast::Shl, Int, Int) => builder.push_binary_inst(Shl, left, right),
-            (ast::Shr, Int, Int) => builder.push_binary_inst(Asr, left, right),
+            (ast::Mul, Float, Float) => self.builder.push_binary_inst(Fmul, left, right),
+            (ast::Add, Int, Int) => self.builder.push_binary_inst(Add, left, right),
+            (ast::Add, Float, Float) => self.builder.push_binary_inst(Fadd, left, right),
+            (ast::Sub, Int, Int) => self.builder.push_binary_inst(Sub, left, right),
+            (ast::Sub, Float, Float) => self.builder.push_binary_inst(Fsub, left, right),
+            (ast::Shl, Int, Int) => self.builder.push_binary_inst(Shl, left, right),
+            (ast::Shr, Int, Int) => self.builder.push_binary_inst(Asr, left, right),
             (ast::BitAnd, Int, Int) |
-            (ast::LogicalAnd, Bool, Bool) => builder.push_binary_inst(And, left, right),
+            (ast::LogicalAnd, Bool, Bool) => self.builder.push_binary_inst(And, left, right),
             (ast::BitOr, Int, Int) |
-            (ast::LogicalOr, Bool, Bool) => builder.push_binary_inst(Or, left, right),
-            (ast::BitXor, Int, Int) => builder.push_binary_inst(Xor, left, right),
+            (ast::LogicalOr, Bool, Bool) => self.builder.push_binary_inst(Or, left, right),
+            (ast::BitXor, Int, Int) => self.builder.push_binary_inst(Xor, left, right),
             (ast::Eq, Int, Int) |
-            (ast::Eq, Bool, Bool) => builder.push_int_comp_inst(Eq, left, right),
-            (ast::Eq, Float, Float) => builder.push_float_comp_inst(Eq, left, right),
+            (ast::Eq, Bool, Bool) => self.builder.push_int_comp_inst(Eq, left, right),
+            (ast::Eq, Float, Float) => self.builder.push_float_comp_inst(Eq, left, right),
             (ast::Ne, Int, Int) |
-            (ast::Ne, Bool, Bool) => builder.push_int_comp_inst(Ne, left, right),
-            (ast::Ne, Float, Float) => builder.push_float_comp_inst(Ne, left, right),
-            (ast::Lt, Int, Int) => builder.push_int_comp_inst(Lt, left, right),
-            (ast::Lt, Float, Float) => builder.push_float_comp_inst(Lt, left, right),
-            (ast::Gt, Int, Int) => builder.push_int_comp_inst(Gt, left, right),
-            (ast::Gt, Float, Float) => builder.push_float_comp_inst(Gt, left, right),
-            (ast::Le, Int, Int) => builder.push_int_comp_inst(Le, left, right),
-            (ast::Le, Float, Float) => builder.push_float_comp_inst(Le, left, right),
-            (ast::Ge, Int, Int) => builder.push_int_comp_inst(Ge, left, right),
-            (ast::Ge, Float, Float) => builder.push_float_comp_inst(Ge, left, right),
+            (ast::Ne, Bool, Bool) => self.builder.push_int_comp_inst(Ne, left, right),
+            (ast::Ne, Float, Float) => self.builder.push_float_comp_inst(Ne, left, right),
+            (ast::Lt, Int, Int) => self.builder.push_int_comp_inst(Lt, left, right),
+            (ast::Lt, Float, Float) => self.builder.push_float_comp_inst(Lt, left, right),
+            (ast::Gt, Int, Int) => self.builder.push_int_comp_inst(Gt, left, right),
+            (ast::Gt, Float, Float) => self.builder.push_float_comp_inst(Gt, left, right),
+            (ast::Le, Int, Int) => self.builder.push_int_comp_inst(Le, left, right),
+            (ast::Le, Float, Float) => self.builder.push_float_comp_inst(Le, left, right),
+            (ast::Ge, Int, Int) => self.builder.push_int_comp_inst(Ge, left, right),
+            (ast::Ge, Float, Float) => self.builder.push_float_comp_inst(Ge, left, right),
             _ => {
                 panic!(
                     "Invalid binary expression '{}' with operand types '{}' and '{}'",
